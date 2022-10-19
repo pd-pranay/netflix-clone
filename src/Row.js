e53e9f97aee713c6ca9b2f97a6bc6dd6f570d774
Pranay Das <pranay@Pranays-MacBook-Air.local>
2022-10-19 20:50:06 +0530

    Add row completed

--- src/Row.js
+++ src/Row.js
@@ -3,6 +3,7 @@ import './Row.css'
 import axios from './axios'
 export default function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
+    const baseURL = 'https://image.tmdb.org/t/p/original/'
     useEffect(() => {
         async function fetchData() {
             const response = axios.get(fetchUrl);
@@ -11,12 +12,29 @@ export default function Row({ title, fetchUrl, isLargeRow = false }) {
         }
         fetchData();
     }, [fetchUrl])
-    console.log(movies)
     return (
         <div className='row'>
-            <h1>
+            <h2>
                 {title}
-            </h1>
+            </h2>
+            <div className='row-posters'>
+
+                {
+                    movies.map(
+                        (movie) =>
+                            ((isLargeRow && movie.poster_path) ||
+                                (!isLargeRow && movie.backdrop_path)) && (
+                                <img
+                                    key={movie.id}
+                                    className={`row-poster ${isLargeRow && "row-poster-large"}`}
+                                    src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                        }`}
+                                    alt={movie.name}
+                                />
+                            )
+
+                    )}
+            </div>
         </div>
     )
 }
